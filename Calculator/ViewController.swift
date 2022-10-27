bfb9054346023ed697cf7ee679aa39cd026a6e2f
Leonardo Cardoso <cardoso.leonardo@outlook.com>
2022-10-27 18:53:39 -0300

    feat: implement AC, negative and percentage functionality

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -12,20 +12,43 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var displayLabel: UILabel!
     
-    
+    private var isFinishedTypingNumber: Bool = true
     
     @IBAction func calcButtonPressed(_ sender: UIButton) {
         
         //What should happen when a non-number button is pressed
-    
+        
+        guard let number = Double(displayLabel.text!) else { fatalError() }
+        
+        if let button = sender.currentTitle {
+            switch button {
+            case "+/-":
+                displayLabel.text = String(number * -1)
+            case "%":
+                displayLabel.text = String(number / 100)
+            case "AC":
+                displayLabel.text = "0"
+            default:
+                displayLabel.text = String(number)
+            }
+        }
+        
+        isFinishedTypingNumber = true
     }
-
+    
     
     @IBAction func numButtonPressed(_ sender: UIButton) {
         
-        displayLabel.text?.append(sender.currentTitle!)
+        if let numValue = sender.currentTitle {
+            if isFinishedTypingNumber {
+                displayLabel.text = numValue
+                isFinishedTypingNumber = false
+            } else {
+                displayLabel.text! += numValue
+            }
+        }
         
     }
-
+    
 }
 
